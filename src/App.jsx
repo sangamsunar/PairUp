8eb4164dbbea2bd1748c7d58b117a75d9e2769a7
Sangam Sunar <s.sangam.00007@gmail.com>
2026-02-09 13:21:30 +0545

    playable

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ const cardValues = [
 function App() {
   const [cards, setCards] = useState([]);
 
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+
   const initializeGame = () => {
     //shuffle the CARDS
 
@@ -56,6 +59,53 @@ function App() {
     });
 
     setCards(newCards);
+    const newFlippedCards = [...flippedCards, card.id];
+    setFlippedCards(newFlippedCards);
+
+    //Check for match if two cards are flipped
+
+    if (flippedCards.length === 1) {
+      const firstCard = cards[flippedCards[0]];
+
+      if (firstCard.value == card.value) {
+        setTimeout(() => {
+          setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
+          const newMatchedCards = cards.map((c) => {
+            if (c.id === card.id || c.id === firstCard.id) {
+              return { ...c, isMatched: true };
+            } else {
+              return c;
+            }
+          });
+          setCards((prev) =>
+            prev.map((c) => {
+              if (c.id === card.id || c.id === firstCard.id) {
+                return { ...c, isMatched: true };
+              } else {
+                return c;
+              }
+            }),
+          );
+          setFlippedCards([]);
+        }, 500);
+      } else {
+        //flip back card 1, card 2
+
+        setTimeout(() => {
+          const flippedBackCard = newCards.map((c) => {
+            if (newFlippedCards.includes(c.id) || c.id === card.id) {
+              return { ...c, isFlipped: false };
+            } else {
+              return c;
+            }
+          });
+
+          setCards(flippedBackCard);
+
+          setFlippedCards([]);
+        }, 1000);
+      }
+    }
   };
 
   return (
