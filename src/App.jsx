8404e72b4d4b294b84ab82611aa1e0af93a8e14f
Sangam Sunar <s.sangam.00007@gmail.com>
2026-02-09 11:39:31 +0545

    working

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,31 @@ function App() {
     initializeGame();
   }, []);
 
+  const handleCardClick = (card) => {
+    // Don't allow clicking if card is already flipped
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    //update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+  };
+
   return (
     <div className="app">
       <GameHeader score={3} moves={10} />
 
       <div className="cards-grid">
         {cards.map((card) => (
-          <Card card={card} />
+          <Card card={card} onClick={handleCardClick} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,9 @@
 export const Card = ({ card, onClick }) => {
   return (
-    <div className="card" onClick={() => onClick(card)}>
+    <div
+      className={`card ${card.isFlipped ? "flipped" : ""}`}
+      onClick={() => onClick(card)}
+    >
       <div className="card-front">?</div>
       <div className="card-back">{card.value}</div>
     </div>
